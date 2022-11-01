8a95f9fb29c9ed2a3ec8fcc647dc5ae5c8adca46
Rayza-Souto <rayzasouto@outlook.com>
2022-11-01 07:54:29 -0300

    Inserindo a classe em um novo pacote para controle

---
+++ java-iniciante/src/edu/rayza/javabasico/TiposVariaveis.java
@@ -0,0 +1,22 @@
+package edu.rayza.javabasico;
+public class TiposVariaveis {
+    public static void main(String[] args) throws Exception {
+
+        double salarioMinimo = 2500.44;
+        //valores acima de 999 não precisa ser separado
+        //o ponto deve ser usado conforme exemplo acima
+
+        //int e long não aceita o zero inicial. precisaria ser string
+
+        // TiposEVariaveis.java
+
+    short numeroCurto = 1;
+    int numeroNormal = numeroCurto;
+    int numeroCurto2 = numeroNormal;
+
+    String meuNome = "Rayza Souto";
+
+        
+
+    }
+}
